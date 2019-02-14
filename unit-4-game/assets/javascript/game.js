6b6bc8d961490af33ce681f1dd79b56a0c0a1998
tyler <tylercedge@gmail.com>
2019-02-13 22:25:39 -0500

    link isnt working

---
+++ unit-4-game/assets/javascript/game.js
@@ -0,0 +1,9 @@
+var randomResult;
+var wins;
+var losses;
+
+for(var i = 0; i < 4; i++){
+    var crystal = $("<div>");
+        crystal.attr("class", 'crystal');
+    $(".crystals").append(crystal);
+}
